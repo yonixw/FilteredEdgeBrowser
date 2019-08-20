3782127e36408cb93ab48727e1e03a6c0060d52b
Yoni Was (yoga-lap) <yonixw@gmail.com>
2019-08-20 20:56:05 +0300

    Making history for each tab, and it being cyclic and drop-future

---
+++ FilteredEdgeBrowser/DataStructure/CyclicFastDrop.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilteredEdgeBrowser.DataStructure
+{
+    class CyclicFastDrop<T> 
+    {
+        T[] _cyclicBuffer = null;
+
+        public CyclicFastDrop(int arrayMaxSize)
+        {
+            _cyclicBuffer = new T[arrayMaxSize];
+        }
+
+        int _currentPosition = 0;
+        int _firstElementPosition = 0;
+        int _nextElementPosition = 0;
+
+        // ================ Helpers ================
+
+        int cyclicDistance(int startIndex, int lastIndex)
+        {
+            if (lastIndex > startIndex)
+            {
+                // The array is like this XX[=====]XX
+                return lastIndex - startIndex;
+            }
+            else
+            {
+                // The array is like this ==]XXXXX[==
+                return (lastIndex - 0) + (_cyclicBuffer.Length - startIndex);
+            }
+        }
+
+        int getCyclicDelta(int delta, int startIndex)
+        {
+            int requestedLocation = delta + startIndex;
+            int bufferSize = _cyclicBuffer.Length;
+
+            if (requestedLocation >= bufferSize)
+            {
+                return requestedLocation % bufferSize;
+            }
+            else if (requestedLocation < 0)
+            {
+                return (requestedLocation % bufferSize) + bufferSize;
+            }
+            else
+            {
+                return requestedLocation;
+            }
+        }
+    
+        int getRelativeToCurrent(int delta)
+        {
+            return getCyclicDelta(delta, _currentPosition);
+        }
+
+        // ================ Public ================
+
+        public int Size()
+        {
+            return cyclicDistance(_firstElementPosition, _nextElementPosition);
+        }
+
+        public void AddAndDropFuture(T newItem)
+        {
+            if (Size() > 0 && !newItem.Equals(_cyclicBuffer[getRelativeToCurrent(0)]))
+            {
+                _nextElementPosition = getRelativeToCurrent(+2);
+                _cyclicBuffer[getRelativeToCurrent(+1)] = newItem;
+                _currentPosition = getRelativeToCurrent(+1);
+            }
+        }
+
+        public int CurrentPosition
+        {
+            get
+            {
+                return cyclicDistance(_firstElementPosition, _currentPosition);
+            }
+            set
+            {
+                if (value > -1 && value < Size())
+                {
+                    _currentPosition = _firstElementPosition;
+                    _currentPosition = getRelativeToCurrent(value);
+                }
+            }
+        }
+
+        public T this[int i]
+        {
+            get {
+                if (i < Size())
+                {
+                    return _cyclicBuffer[getCyclicDelta(i, _firstElementPosition)];
+                }
+                else
+                {
+                    throw new Exception("Requested index " + i + " is out of bounds ("+ Size() +")");
+                }
+            }
+        }
+
+    }
+}
---
+++ FilteredEdgeBrowser/LocalHistoryManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilteredEdgeBrowser
+{
+    class HistoryItem
+    {
+        public Uri URL;
+        public string Title;
+
+        public override bool Equals(object obj)
+        {
+            HistoryItem other = obj as HistoryItem;
+            if (other == null)
+                return false;
+
+
+            return URL.Equals(other.URL);
+        }
+    }
+
+    class LocalHistoryManager
+    {
+        DataStructure.CyclicFastDrop<HistoryItem> myCyclicHistory 
+                    = new DataStructure.CyclicFastDrop<HistoryItem>(25);
+
+        public void Navigated(Uri url, string title)
+        {
+            HistoryItem newItem = new HistoryItem() { URL = url, Title = title };
+            myCyclicHistory.AddAndDropFuture(newItem);            
+        }
+
+        public void NavigatedIndex(int i)
+        {
+            myCyclicHistory.CurrentPosition = i;
+        }
+
+        public int Size()
+        {
+            return myCyclicHistory.Size();
+        }
+
+        public Uri GoBack()
+        {
+            if (Size() > 0 && myCyclicHistory.CurrentPosition > 0)
+            {
+                myCyclicHistory.CurrentPosition--;
+                return myCyclicHistory[myCyclicHistory.CurrentPosition].URL;
+            }
+
+            return null;
+        }
+
+        public Uri GoForward()
+        {
+            if (Size() > 0 && myCyclicHistory.CurrentPosition < (Size()-1))
+            {
+                myCyclicHistory.CurrentPosition++;
+                return myCyclicHistory[myCyclicHistory.CurrentPosition].URL;
+            }
+
+            return null;
+        }
+
+        public HistoryItem this[int i]
+        {
+            get
+            {
+                return myCyclicHistory[i];
+            }
+        }
+    }
+}
