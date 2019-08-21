1c342ef563503d6e4c16e715c2a940a414c0d729
Yoni Was (yoga-lap) <yonixw@gmail.com>
2019-08-21 21:39:41 +0300

    Starting edit url dialog

---
+++ FilteredEdgeBrowser/Dialogs/frmEditUrl.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace FilteredEdgeBrowser.Dialogs
+{
+    partial class frmEditUrl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.txtFreeStyle = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inline edit:";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(16, 29);
+            this.txtURL.Multiline = true;
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(537, 53);
+            this.txtURL.TabIndex = 2;
+            // 
+            // txtFreeStyle
+            // 
+            this.txtFreeStyle.Location = new System.Drawing.Point(16, 118);
+            this.txtFreeStyle.Name = "txtFreeStyle";
+            this.txtFreeStyle.Size = new System.Drawing.Size(597, 26);
+            this.txtFreeStyle.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 97);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Freestyle Edit:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox2);
+            this.groupBox1.Location = new System.Drawing.Point(16, 214);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(597, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "History search";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox3);
+            this.groupBox2.Location = new System.Drawing.Point(16, 320);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(597, 114);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Bookmark Search";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listBox4);
+            this.groupBox3.Location = new System.Drawing.Point(16, 451);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(597, 130);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Google suggestion";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(16, 151);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(597, 58);
+            this.listBox1.TabIndex = 8;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 18;
+            this.listBox2.Location = new System.Drawing.Point(6, 25);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(585, 58);
+            this.listBox2.TabIndex = 9;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 18;
+            this.listBox3.Location = new System.Drawing.Point(6, 25);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(585, 76);
+            this.listBox3.TabIndex = 10;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 18;
+            this.listBox4.Location = new System.Drawing.Point(6, 25);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(585, 94);
+            this.listBox4.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(560, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 53);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "◄ OK!";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // frmEditUrl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 593);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtFreeStyle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmEditUrl";
+            this.ShowInTaskbar = false;
+            this.Text = "Edit the url";
+            this.Load += new System.EventHandler(this.frmEditUrl_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.TextBox txtFreeStyle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
